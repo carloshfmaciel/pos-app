c98d093ccfb37a3a1937502df102977624c595d9
Carlos Maciel <carlos.maciel@sbwebservices.net>
2022-10-10 23:23:06 -0300

    adjust and test schedule delivery controller with rest endpoints

---
+++ src/main/java/net/posapp/constants/ScheduleDeliveryStatus.java
@@ -0,0 +1,10 @@
+package net.posapp.constants;
+
+public abstract class ScheduleDeliveryStatus {
+
+	public static final String ACTIVE = "A";
+
+	public static final String INACTIVE = "I";
+	
+	public static final String DELIVERED = "D";
+}
