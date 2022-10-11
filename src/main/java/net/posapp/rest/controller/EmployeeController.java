43a9f984cd06580d9b8c4c7d933396ec694cd841
Carlos Maciel <carlos.maciel@sbwebservices.net>
2022-10-11 02:01:21 -0300

    update postman collection

--- src/main/java/net/posapp/rest/controller/EmployeeController.java
+++ src/main/java/net/posapp/rest/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.posapp.constants.RolesConst;
 import net.posapp.rest.request.EmployeeRequest;
 import net.posapp.service.EmployeeService;
 
@@ -36,18 +38,21 @@ public class EmployeeController {
 		return ResponseEntity.status(HttpStatus.OK).body(employeeRequest);
 	}
 
+	@Secured({RolesConst.ROLE_ADMIN})
 	@PostMapping(path = "/")
 	public ResponseEntity<EmployeeRequest> insert(@RequestBody EmployeeRequest employeeRequest) {
 		employeeService.save(employeeRequest);
 		return ResponseEntity.status(HttpStatus.CREATED).body(employeeRequest);
 	}
 
+	@Secured({RolesConst.ROLE_ADMIN})
 	@PutMapping(path = "/")
 	public ResponseEntity<EmployeeRequest> update(@RequestBody EmployeeRequest employeeRequest) {
 		employeeService.save(employeeRequest);
 		return ResponseEntity.status(HttpStatus.OK).body(employeeRequest);
 	}
 
+	@Secured({RolesConst.ROLE_ADMIN})
 	@DeleteMapping(path = "/{id}")
 	public ResponseEntity inative(@PathVariable Integer id) {
 		employeeService.inactive(id);
